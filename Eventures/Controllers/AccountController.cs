726b877e11f6b9e0e116ee8ec31bd801604f9938
Nackey <s2k@abv.bg>
2018-11-18 22:13:35 +0200

    Add Eventures project with two styled views

---
+++ Eventures/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eventures.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventures.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Eventures/Models/EventureUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventures.Models
+{
+    public class EventureUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UniqueCitizenNumber { get; set; }
+    }
+}
